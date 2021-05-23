f1b9bc2029d0253d653fd2a0fedcdb5e362dcb59
Christine Julien <christine.julien@gmail.com>
2021-05-23 13:34:52 -0500

    cleanup

--- projects/banana/main.ts
+++
@@ -1,9 +0,0 @@
-input.onButtonPressed(Button.A, function () {
-    moveSMART.startCounting()
-})
-input.onButtonPressed(Button.B, function () {
-    moveSMART.stopCounting()
-})
-basic.forever(function () {
-    moveSMART.showStepCount()
-})
